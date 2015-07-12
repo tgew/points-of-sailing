b62bdbfecd5afecebc515148547897ad3dbb5d5c
tgew <tim@goosemonkey.com>
2015-07-12 16:19:01 -0400

    radio buttons working.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
     
     $("#welcome").fadeIn(1000);
@@ -5,26 +7,24 @@ $(document).ready(function() {
 
   	$("#welcome").click(function(){
   		$("#welcome").fadeOut(500);
-        $("#quiz_page").fadeIn(500);
+        $("#quiz_view").fadeIn(500);
   	});
     
+    $("#submit_button").click(function(){
+        var radios = document.getElementsByTagName('input');
+        var value;
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].type === 'radio' && radios[i].checked) {
+                value = radios[i].value;  
+                console.log(value);
+            }
+        }
+        $("#quiz_view").fadeOut(500);
+        $("#status_card").fadeIn(500);
+    });
+    
 
-  	
-    
-    
-    
-    
-    
-    
-//    $("#prep_page").click(function(){
-//  		$("#prep_page").fadeOut(500);
-//        $("#quiz_page").fadeIn(500);
-//        $("#quiz_form").submit(function(event){
-//            var $answer = this.val();
-//            console.log($answer);
-//            event.preventDefault();
-//        });
-//  	});    
+  	  
     
     
 });  // Document Ready closure
