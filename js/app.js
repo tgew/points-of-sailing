7cd4bf629a500ec4270748578bf81b9dd1283a86
tgew <tim@goosemonkey.com>
2015-07-12 11:58:27 -0400

    very beginnings of interactivity.  testing out my modals.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
+    
+    $("#welcome").fadeIn(1000);
 
 
+  	$("#welcome").click(function(){
+  		$("#welcome").fadeOut(500);
+        $("#quiz_page").fadeIn(500);
+  	});
+    
+
+  	
+    
+    
+    
+    
+    
+    
+//    $("#prep_page").click(function(){
+//  		$("#prep_page").fadeOut(500);
+//        $("#quiz_page").fadeIn(500);
+//        $("#quiz_form").submit(function(event){
+//            var $answer = this.val();
+//            console.log($answer);
+//            event.preventDefault();
+//        });
+//  	});    
+    
     
 });  // Document Ready closure
