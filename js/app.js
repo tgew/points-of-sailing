53dde7ddff2ecc467f87588a79f5f0c54a8f253c
tgew <tim@goosemonkey.com>
2015-07-18 10:34:57 -0400

    big changes. created score object to track quiz score. collapsed image display and answer choices display functions into quize function.  score is not displaying correct numbers.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,36 @@
-var answer;
-var correctAnswers = 0;
-var currentQuestion = 0;
+var score = {
+    numberCorrect: 0,
+    workingQuestion: 0,
+    percentCorrect: function(){
+        return Math.floor(100*(this.numberCorrect/this.workingQuestion))
+    }
+}
 
 function makeQuizQuestion() {
     
     var QuizQuestion = {};
     
 // Since we're creating the next question, increment the currentQuestion number.
-    currentQuestion++;
+    score.workingQuestion++;
 
 // Pick and answer and display the image for it.
     QuizQuestion.answer = Math.floor(Math.random()*6);
-    displayImage(QuizQuestion.answer);
+//    displayImage(QuizQuestion.answer);
 
 
-    var chooseFrom = [0, 1, 2, 3, 4, 5]
+    var chooseFrom = [0, 1, 2, 3, 4, 5];
     var questionsArray = [];
     var chooseIndex;
 
 // Push the correct answer into the questions array.    
     questionsArray.push(QuizQuestion.answer);
-    for(var i = chooseFrom.length - 1; i >= 0; i--) {
-        if(chooseFrom[i] === answer) {
+    for (var i = chooseFrom.length - 1; i >= 0; i--) {
+        if(chooseFrom[i] === QuizQuestion.answer) {
             chooseFrom.splice(i, 1);
         }
     }
 
-    while(questionsArray.length < 4) {
+    while (questionsArray.length < 4) {
         chooseIndex = Math.floor(Math.random()*chooseFrom.length);
         questionsArray.push(chooseFrom[chooseIndex]);
         chooseFrom.splice(chooseIndex, 1);    
@@ -36,13 +40,16 @@ function makeQuizQuestion() {
     questionsArray.push(chooseFrom[0]);
     
 // Now we should have questionArray filled with options and one of them is the answer.
-    QuizQuestion.questionsArray = questionsArray
+    QuizQuestion.questionsArray = questionsArray;
     
 return QuizQuestion;
 }
 
-function displayImage(imageNumber) {
-    switch(imageNumber) {
+function displayQuiz(question) {
+    var itemLabels = ["In irons", "Close hauled", "Close reach", "Beam reach", "Broad reach", "Running"];
+
+// Display the image of the answer.
+    switch(question.answer) {
         case 0:
             $('#quiz_view > img').attr("src", "./images/in_irons.png");
             break;
@@ -61,21 +68,18 @@ function displayImage(imageNumber) {
         default:
             $('#quiz_view > img').attr("src", "./images/running.png");
     }
-}
-
-function displayQuestions(questionList){
-    var itemLabels = ["In irons", "Close hauled", "Close reach", "Beam reach", "Broad reach", "Running"]
-    $("#question1").next().html(itemLabels[questionList[0]]);
-    $("#question2").next().html(itemLabels[questionList[1]]);                                         
-    $("#question3").next().html(itemLabels[questionList[2]]);
-    $("#question4").next().html(itemLabels[questionList[3]]);                                         
+    
+// Display the answer choices.    
+    $("#question1").next().html(itemLabels[question.questionsArray[0]]);
+    $("#question2").next().html(itemLabels[question.questionsArray[1]]);
+    $("#question3").next().html(itemLabels[question.questionsArray[2]]);
+    $("#question4").next().html(itemLabels[question.questionsArray[3]]);       
 }
 
 function displayStats(answerValue){
-    var percentRight = Math.floor(100*(correctAnswers/currentQuestion));
-    $("#numberRight").text(correctAnswers);
-    $(".totalQuestions").text(currentQuestion);
-    $("#percentage").text(percentRight);
+    $("#numberRight").text(score.numberCorrect);
+    $(".totalQuestions").text(score.workingQuestion);
+    $("#percentage").text(score.percentCorrect());
     if (answerValue) {
         $("#statusGreeting").text("Great job, mate.  You got it right.");
     } else {
@@ -86,45 +90,38 @@ function displayStats(answerValue){
 $(document).ready(function() {
     
     $("#welcome").fadeIn(1000);
+// Create a new quiz object    
+    var firstQuestion = makeQuizQuestion();
+    displayQuiz(firstQuestion);
 
-
-  	$("#welcome").click(function(){
+    $("#welcome").on('click', function() {
   		$("#welcome").fadeOut(10);
-        $("#quiz_view").fadeIn(10);
-        // create a new quiz object
-        var quizQuestion1 = makeQuizQuestion();
-        answer = quizQuestion1.answer;
-        displayImage(quizQuestion1.answer);
-        displayQuestions(quizQuestion1.questionsArray);
-  	});
+        $("#quiz_view").fadeIn(10);    
+    });    
     
-    $("#submit_button").click(function(){
+    
+    $("#submit_button").on('click', function(){
         var radios = document.getElementsByTagName('input');
         var value;
-        var answerValue = false;
+        var correctAnswer = false;
         for (var i = 0; i < radios.length; i++) {
             if (radios[i].type === 'radio' && radios[i].checked) {
                 value = radios[i].value;  
                 radios[i].checked = false;
-                if (value == answer) {
-                    correctAnswers++;
-                    answerValue = true;
+                if (value == firstQuestion.answer) {
+                    score.numberCorrect++;
+                    correctAnswer = true;
                 }
-                
                 $("#quiz_view").fadeOut(10);
                 $("#status_card").fadeIn(10);
-                displayStats(answerValue);                
+                displayStats(correctAnswer);                
             }
         }
     });
     
-    $("#status_card").click(function(){
+    $("#status_card").on('click', function(){
         $("#status_card").fadeOut(10);   
-        $("#quiz_view").fadeIn(10);
-        var quizQuestion1 = makeQuizQuestion();
-        answer = quizQuestion1.answer;
-        displayImage(quizQuestion1.answer);
-        displayQuestions(quizQuestion1.questionsArray);        
+        $("#quiz_view").fadeIn(10);    
     });
     
     
